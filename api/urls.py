60a3aae3c8398e2b721056d918647f67d5451f91
Çağatay Onur Şengör <contact@csengor.com>
2020-07-22 22:09:59 +0300

    Fixed a typo

--- api/urls.py
+++ api/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('project/<int:project_id>', views.project_files, name='project_files'),
     path('project/<int:project_id>/file/<int:file_id>', views.project_file, name='project_file'),
     path('project/new', views.new_project, name='new_project'),
-    #path('project/import', views.inport_project, name='import_project'),
+    path('project/import', views.import_project, name='import_project'),
     path('tms', views.tm_directory, name='tm_directory'),
     path('tm/new', views.new_tm, name='new_tm'),
 ]
