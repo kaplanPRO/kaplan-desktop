3d0234d916c38d9b213f7db40b29b5ece2b2ef00
Çağatay Onur Şengör <contact@csengor.com>
2020-10-11 08:05:49 +0300

    Correct name for files view

--- app/src/batch-tasks.js
+++ app/src/batch-tasks.js
@@ -1,6 +1,6 @@
 function getTargetTranslation(fileId) {
     $.post(
-        "http://127.0.0.1:8000/project/" + $(files_view).attr("cur-p-id") + "/file/" + fileId,
+        "http://127.0.0.1:8000/project/" + filesView.getAttribute("cur-p-id") + "/file/" + fileId,
         {
             task: "generate_target_translation",
         }
